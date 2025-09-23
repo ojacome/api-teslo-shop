b8ea17fd11c578b412050dbd17d0f95745928d42
Jesus Jacome <olmedo.bdp@gmail.com>
2025-09-23 11:58:40 -0500

    crear producto

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -6,5 +6,37 @@ export class Product {
     id: number
 
     @Column('text', { unique: true })
-    tittle: string
+    title: string
+
+    @Column('float', { default: 0 })
+    price: number
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string
+
+    @Column({
+        type: 'text',
+        unique: true
+    })
+    slug: string
+
+    @Column({
+        type: 'int',
+        default: 0
+    })
+    stock: number
+
+    @Column({
+        type: 'text',
+        array: true
+    })
+    sizes: string[]
+
+    @Column({
+        type: 'text'
+    })
+    gender: string
 }
